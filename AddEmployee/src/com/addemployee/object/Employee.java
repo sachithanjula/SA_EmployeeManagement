11a776d05189e5ee4841f57a5b4a9ac3289a046c
sasi02 <hksasindu@02>
2019-03-27 02:23:46 +0530

    Add Employee initial Commit

---
+++ AddEmployee/src/com/addemployee/object/Employee.java
@@ -0,0 +1,51 @@
+package com.addemployee.object;
+
+public class Employee {
+	private String firstName;
+	private String secondName;
+	private int age;
+	private double salary;
+	private String designation;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getSecondName() {
+		return secondName;
+	}
+	
+	public void setSecondName(String secondName) {
+		this.secondName = secondName;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public double getSalary() {
+		return salary;
+	}
+	
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+	public String getDesignation() {
+		return designation;
+	}
+	
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	
+	
+}
