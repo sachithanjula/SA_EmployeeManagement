289927e441595e30d22e1558d52218835edc5aab
it17050104 <it17050104@DESKTOP-MHQCGNA>
2019-03-30 22:31:52 +0530

    CHange

--- EmployeeClient/src/employeeclient/Activator.java
+++ EmployeeClient/src/employeeclient/Activator.java
@@ -8,8 +8,6 @@ import org.osgi.framework.BundleContext;
 
 import com.addemployee.object.Employee;
 
-import projects.project.Project;
-
 public class Activator implements BundleActivator {
 
 	private static BundleContext context;
@@ -28,29 +26,28 @@ public class Activator implements BundleActivator {
 		Activator.context = bundleContext;
 		System.out.println("Starting Employee Client.....");
 		
-//		Scanner scanner = new Scanner(System.in);
-//		
-//		System.out.print("Enter First Name:- ");
-//		emp.setFirstName(scanner.nextLine());
-//		
-//		System.out.print("Enter Second Name:- ");
-//		emp.setSecondName(scanner.nextLine());
-//		
-//		System.out.print("Enter Age:- ");
-//		emp.setAge(scanner.nextInt());
-//		
-//		System.out.print("Enter Salary:- ");
-//		emp.setSalary(scanner.nextDouble());
-//		
-//		System.out.print("Enter Designation:- ");
-//		emp.setDesignation(scanner.nextLine());
-//		
-//		empList.add(emp);
-//		
-//		System.out.println(empList.toString());
-//		
+		Scanner scanner = new Scanner(System.in);
 		
-		Project p = new Project(null, null, null, null, null, null);
+		System.out.print("Enter First Name:- ");
+		emp.setFirstName(scanner.nextLine());
+		
+		System.out.print("Enter Second Name:- ");
+		emp.setSecondName(scanner.nextLine());
+		
+		System.out.print("Enter Age:- ");
+		emp.setAge(scanner.nextInt());
+		
+		System.out.print("Enter Salary:- ");
+		emp.setSalary(scanner.nextDouble());
+		
+		System.out.print("Enter Designation:- ");
+		emp.setDesignation(scanner.nextLine());
+		
+		scanner.close();
+		
+		empList.add(emp);
+		
+		System.out.println(empList.toString());
 		
 		System.out.println("Started Employee Client!");
 	}
